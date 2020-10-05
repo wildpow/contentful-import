b34905c942a86d6292ba3133ca0189d3cc0c6a16
wildpow <element1028@gmail.com>
2020-10-05 00:48:56 -0700

    feat: dotenv, commitlint and contentful api working

---
+++ main.js
@@ -0,0 +1,16 @@
+require("dotenv").config();
+var contentful = require("contentful");
+console.log(process.env);
+var client = contentful.createClient({
+  space: process.env.SPACEID,
+  accessToken: process.env.ACCESSTOKEN,
+});
+
+// Test
+
+client
+  .getEntries()
+  .then((entries) => {
+    entries.items.forEach((entry) => console.log(entry));
+  })
+  .catch((err) => console.log(err));
